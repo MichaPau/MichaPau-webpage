5db21adb5a2485b4d0d3d0a3e8511511ecf2965b
MichaPau <raspar_123@yahoo.fr>
2016-09-05 14:26:26 +0200

    update 2016-09-05

--- _posts/2016-03-16-initial-release.md
+++ _posts/2016-03-16-initial-release.md
@@ -8,7 +8,7 @@ description: I finally come up with my own web page.
 
 # So after years (more than ten actually) as a freelance worker, I finally come up with my own web page.  
 It take me so long because before I always had fixed clients and there was no need for self promotion or to be _googleable_ by any means.  
-Now, I am looking for more individual projects and to include more creativity to my work.
+Now, I am looking for more individual projects and to include more creativity in my work.
 
 I am from Vienna, Austria but I live since some years in Rennes, in the beautiful region of Brittany/France.
 
@@ -16,9 +16,4 @@ So why is this page in English when I am in a francophone country?
 
 It's because I want to keep it international and even French people with their _a bit funny_ but _beautiful_ accent do speak English!
 
-A translation will come one day anyway. 
-
-
-
-
-
+A translation will come one day anyway.
--- assets/php/mail.php
+++ assets/php/mail.php
@@ -1,20 +1,23 @@
 <?php
-	
+
 	$name = $_POST['name'];
 	$email = $_POST['email'];
-	/*if( $_POST['email'] && !preg_match( "/[\r\n]/", $_POST['email']) ) {
-		$email = $_POST['email'];
-	} else {
-		$email = "contact@michapau.net";
-	}*/
-	
+
+
 	$subject = $_POST['subject'];
+	$subject = str_replace(array("\r","\n"),array(" "," "), $subject);
 	$message = $_POST['message'];
-	$formcontent="From: $name - eMail: $email \n Message: \n $message";
-	$recipient = "contact@michapau.net, michael_kittenberger@yahoo.fr";
-	$mailheader = "From: contact@michapau.net \r\n";
+	$formcontent="From: $name - eMail: $email \n Subject: $subject \n Message: \n $message";
+	$recipient = "contact@michapau.net";
+	//$mailheader = "From: contact@michapau.net \r\n";
+	$headers   = array();
+	$headers[] = "MIME-Version: 1.0";
+	$headers[] = "Content-type: text/plain; charset=iso-8859-1";
+	$headers[] = "Bcc: michael_kittenberger@yahoo.fr";
+	$headers[] = "Subject: mail from webpage";
+	$headers[] = "X-Mailer: PHP/".phpversion();
 	if(isset($_POST['url']) && $_POST['url'] == ''){
-		$mail_ret = mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+		$mail_ret = mail($recipient, $subject, $formcontent, implode("\r\n", $headers)) or die("Error!");
 	} else {
 		$mail_ret = 0;
 	}
